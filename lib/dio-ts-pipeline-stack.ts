8ec1d86338d7034e36fe2e1ff21bf319f0a14dbb
Brent Fox <brentgfox@gmail.com>
2022-01-27 18:34:05 -0700

    Updated per code base.

--- lib/dio-ts-pipeline-stack.ts
+++ lib/dio-ts-pipeline-stack.ts
@@ -1,16 +1,18 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class DioTsPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DioTsPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
