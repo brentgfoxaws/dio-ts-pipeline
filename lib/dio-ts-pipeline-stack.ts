662278d5c0ef6f7b3f98937ef31ea4f2b05f941b
Brent Fox <brentgfox@gmail.com>
2022-01-27 18:48:07 -0700

    Added Codestar reference

--- lib/dio-ts-pipeline-stack.ts
+++ lib/dio-ts-pipeline-stack.ts
@@ -1,18 +1,33 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, CodeBuildStep, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class DioTsPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
+
+      pipelineName: "BlogPipeline",
+			synth: new CodeBuildStep("SynthStep", {
+				input: CodePipelineSource.connection(
+					"brentgfoxaws/dio-ts-pipeline",
+					"main",
+					{
+						connectionArn:
+							"arn:aws:codestar-connections:us-west-2:492199546644:connection/5054d256-4253-4d4b-aeed-3828a581ffba"
+					}
+				),
+				installCommands: ["npm install -g aws-cdk"],
+				commands: ["npm ci", "npm run build", "npx cdk synth"]
+			})
+      /*
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
-    });
+      })x
+    });*/
   }
 }
